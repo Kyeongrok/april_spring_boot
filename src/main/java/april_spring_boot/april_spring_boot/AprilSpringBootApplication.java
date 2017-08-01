7a5ae031288522f40e221d1338999b16d1c45439
Yjw <ynate519@gmail.com>
2017-08-01 20:23:01 +0900

    no message

---
+++ src/main/java/april_spring_boot/april_spring_boot/AprilSpringBootApplication.java
@@ -0,0 +1,12 @@
+package april_spring_boot.april_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AprilSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AprilSpringBootApplication.class, args);
+	}
+}
